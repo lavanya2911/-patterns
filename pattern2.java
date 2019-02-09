578c847dffe1fb45c16b072c217ee0a603a22684
lavanya2911 <46344347+lavanya2911@users.noreply.github.com>
2019-02-09 14:26:18 +0530

    Update pattern2.java

--- pattern2.java
+++ pattern2.java
@@ -4,11 +4,11 @@ class pattern1
 	public static void main(String [] args)
 	{
 		int a=5;
-		for(int i=1;i<=a;i++)
+		for(int i=0;i<a;i++)
 		{
-			for(int j=1;j<=a;j++)
+			for(int j=0;j<a;j++)
 		{
-			if(j<=(a-i))
+			if(j<(a-i))
 			System.out.print("*");
 		}
 		System.out.println();
