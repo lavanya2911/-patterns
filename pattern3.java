1e63d7ebdb5332921fde598ca007e6b53624951e
lavanya2911 <46344347+lavanya2911@users.noreply.github.com>
2019-02-09 12:12:41 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class pattern3
+{
+	public static void main(String [] args)
+	{
+		int a=5;
+		for(int i=1;i<=a;i++)
+		{
+			for(int j=1;j<=a;j++)
+		{
+			if(j>(a-i))
+			System.out.print("*");
+		}
+		System.out.println();
+		}
+	}
+}
